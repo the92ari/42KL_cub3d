741b7c1baa3f007b1581dc12c42e986f7488f977
David Wang <devtesting404@gmail.com>
2022-09-23 22:07:34 +0800

    add basic_map.cub

--- src/includes/cub3d.h
+++ src/includes/cub3d.h
@@ -6,6 +6,6 @@ void	error_handler(const char *msg, const char *func, int err_no);
 
 // file_validator.c
 void	check_valid_ext(const char *filename, const char *ext_to_check);
-// void	parse_map(char *filename, t_map *map);
+void	parse_map(const char *filename, t_map *map);
 
 #endif
