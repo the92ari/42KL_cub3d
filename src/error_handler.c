26694fb36ed5641648cca031b4307b0611d530c6
David Wang <devtesting404@gmail.com>
2022-09-24 16:46:57 +0800

    Change error_handler to output to stderr

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 21:57:30 by kwang             #+#    #+#             */
-/*   Updated: 2022/09/23 21:57:33 by kwang            ###   ########.fr       */
+/*   Updated: 2022/09/24 14:13:02 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	error_handler(const char *msg, const char *func_name, int err_no)
 {
 	if (err_no)
 		errno = err_no;
-	printf("Error\n");
-	printf("%s\n", msg);
-	perror(func_name);
+	perror("Error");
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	ft_putstr_fd(func_name, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
