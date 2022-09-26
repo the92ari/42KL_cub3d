e956137cdb2cf7e8aaf43c6e7f62c7560425c6a6
David Wang <devtesting404@gmail.com>
2022-09-26 15:13:36 +0800

    Change error_handler's errno to be overwritable if errno not set

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 21:57:30 by kwang             #+#    #+#             */
-/*   Updated: 2022/09/24 14:13:02 by kwang            ###   ########.fr       */
+/*   Updated: 2022/09/26 15:11:05 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	error_handler(const char *msg, const char *func_name, int err_no)
 {
-	if (err_no)
+	if (errno == 0)
 		errno = err_no;
 	perror("Error");
 	ft_putstr_fd(msg, STDERR_FILENO);
