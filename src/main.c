cf8333e0dba20a8d39ad64e019922615e8da3574
David Wang <devtesting404@gmail.com>
2022-09-28 00:30:56 +0800

    Add compatibility for caching textures with mlx functions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 21:57:25 by kwang             #+#    #+#             */
-/*   Updated: 2022/09/27 22:22:54 by kwang            ###   ########.fr       */
+/*   Updated: 2022/09/27 22:43:05 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main(int argc, char **argv){
 
-	t_vars vars;
+	t_config config;
 
 	if (argc != 2)
 	{
 		printf("Usage: %s [<MAP_NAME>.cub]\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-	parse_config(argv[1], &vars);
+	parse_config(argv[1], &config);
 
 	return EXIT_SUCCESS;
 }
